56a533772450b12567c5945096327a8a537ccf73
Aryainguz <aryaninguz369@gmail.com>
2024-09-17 13:47:34 +0530

    refactor: Update styling in pricing and skills components

--- src/components/skills/Skill.tsx
+++ src/components/skills/Skill.tsx
@@ -83,17 +83,17 @@ const skills = [
 const Skills = () => {
   return (
     <>
-    <section className="bg-gray-900">
+    <section className="bg-transparent dark:bg-gray-900">
     <div className="max-w-7xl mx-6 px-4 sm:px-6 sm:mx-auto lg:px-8">
         <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
-        <div className="text-white mb-4 lg:text-center">
+        <div className="text-black dark:text-white mb-4 lg:text-center">
         <h2 className="text-base text-accent font-semibold tracking-wide uppercase"  data-aos="fade-in"
           data-aos-duration="3000">
-          My Expertise
+          My Skills
         </h2>
-        <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight sm:text-4xl"  data-aos="fade-in"
+        <p className="mt-2 text-3xl leading-8 text-gray-700 dark:text-white font-medium tracking-tight sm:text-4xl"  data-aos="fade-in"
           data-aos-duration="3000">
-          {"{"} Expertise {"}"}
+          {"{"} Skills {"}"}
         </p>
       </div>
       </div>
@@ -101,7 +101,7 @@ const Skills = () => {
           data-aos-duration="3000">
             <div className="grid mb-6 grid-cols-12 gap-8 sm:gap-12 md:grid-cols-12 lg:grid-cols-12 text-gray-400">
               {skills.map((skill,i) => (
-                <a href='' key={i} className="text-xl sm:text-3xl text-white flex justify-center items-center ml-4">
+                <a href='' key={i} className="text-xl sm:text-3xl text-black dark:text-white flex justify-center items-center ml-4">
                   {skill.icon}
                 </a>
               ))}
